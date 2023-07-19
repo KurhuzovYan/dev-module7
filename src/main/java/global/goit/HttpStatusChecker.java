b4e315137ae8126aee4c48e6c3472b736222b520
Yan Kurhuzov <yan.dasty@gmail.com>
2023-07-20 00:15:44 +0300

    Commit 4: Refactored code

--- src/main/java/global/goit/HttpStatusChecker.java
+++ src/main/java/global/goit/HttpStatusChecker.java
@@ -1,28 +1,30 @@
 package global.goit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.BasicConfigurator;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 
 public class HttpStatusChecker {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpStatusChecker.class);
-
     public String getStatusImage(int code) {
+        BasicConfigurator.configure();
         try {
             URL url = new URL(String.format("https://http.cat/%s.jpg", code));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
             if (connection.getResponseCode() == 404) {
-                LOGGER.error("Error 404! Please enter correct status code:");
+                throw new IllegalArgumentException("Error 404! Please give correct status code!");
             }
         } catch (IOException e) {
-            LOGGER.error("Error = ", e);
+            throw new RuntimeException("Error = ", e);
         }
         return String.format("https://http.cat/%s.jpg", code);
+    }
+
+    public static void main(String[] args) {
+        HttpStatusChecker checker = new HttpStatusChecker();
+        System.out.println(checker.getStatusImage(2000));
     }
 }
